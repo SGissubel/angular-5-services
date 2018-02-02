1dcac510636f81a52459294bc2dda4af68ece46d
Steven Gissubel <stevengissubel@rb-mac-007.home>
2018-02-02 08:01:36 -0500

    added services to pass data - added emitter/subscribing via child

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,23 +1,20 @@
 import { Component, Input } from '@angular/core';
 
-import { LoggingService } from '../logging.service';
 import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
 
-  constructor(private loggingService: LoggingService,
-              private accountsSevice: AccountsService) {}
+  constructor(private accountsService: AccountsService) {}
 
   onSetTo(status: string) {
-    this.accountsSevice.updateStatus(this.id, status);
-    this.loggingService.logStatusChange(status);
+    this.accountsService.updateStatus(this.id, status);
+    this.accountsService.statusUpdated.emit(status);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+// local components
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+// local services
+import { AccountsService } from './accounts.service';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,7 @@ import { NewAccountComponent } from './new-account/new-account.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [AccountsService, LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
